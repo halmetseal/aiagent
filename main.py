2aee7632a8e5abf2f7b6da75bb1772591124512e
halmetseal <omlette87@proton.me>
2025-08-10 12:51:20 +0200

    Added --verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -11,10 +12,13 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-
     # Just the args stuff
     argv = sys.argv
     prompt = ""
+    verbose = False
+    if len(argv) > 2:
+        if "--verbose" in argv[2:]:
+            verbose = True
     if len(argv) > 1:
         prompt = argv[1]
     else:
@@ -24,12 +28,22 @@ def main():
     # Program begins here
     print("Hello from ai-agent!\n")
 
+    if verbose:
+        print(f"User prompt: {prompt}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt)
+        contents=messages
+    )
     print(f"Gemini: {response.text}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {
+              response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
